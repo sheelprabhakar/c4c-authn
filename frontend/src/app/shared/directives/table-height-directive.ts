16bebfd075cfa1a0b8646d14f9a8f62a6cd04669
Sheel Sindhu Prabhakar <sheel.prabhakar@gmail.com>
2024-09-01 17:06:48 +0530

    refactor: Adjust table height calculation in TableHeightDirective
    - Update the calculation of table height in the TableHeightDirective to account for changes in the content component's padding and height.
    - Add a subscription to router events to recalculate the table height when the navigation or resolve events occur.
    - Unsubscribe from the router events subscription in the ngOnDestroy method to prevent memory leaks.

--- frontend/src/app/shared/directives/table-height-directive.ts
+++ frontend/src/app/shared/directives/table-height-directive.ts
@@ -1,26 +1,27 @@
-import {
-  Directive,
-  ElementRef,
-  Renderer2,
-  HostListener,
-  AfterViewInit,
-  Input,
-} from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener, AfterViewInit, Input, OnDestroy, } from '@angular/core';
+import { Router, NavigationEnd, ResolveEnd } from '@angular/router';
+import { filter, Subscription } from 'rxjs';
 
 @Directive({
   selector: '[tableHeight]',
   standalone: true,
 })
-export class TableHeightDirective implements AfterViewInit {
+export class TableHeightDirective implements AfterViewInit, OnDestroy {
   @Input() minHeight: number;
 
-  constructor(
-    private el: ElementRef,
-    private renderer: Renderer2
-  ) {}
+  private routerSubscription: Subscription;
+  constructor(private el: ElementRef, private renderer: Renderer2, private router: Router
+  ) {
 
+  }
   ngAfterViewInit() {
     this.setHeight();
+    this.routerSubscription = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd || event instanceof ResolveEnd)
+    ).subscribe(event => {
+      console.log('Navigation or Resolve event:', event);
+      this.setHeight();
+    });
   }
 
   @HostListener('window:resize')
@@ -29,13 +30,20 @@ export class TableHeightDirective implements AfterViewInit {
   }
 
   private setHeight() {
-    let height =
-      this.el.nativeElement.closest('.content-c4c').offsetHeight - 235;
+    const element = this.el.nativeElement.closest('.content-c4c');
+    const viewportHeight = window.innerHeight;
+    const elementOffsetTop = element.getBoundingClientRect().top;
+    let height = viewportHeight - elementOffsetTop -285;
 
     if (this.minHeight && height < this.minHeight) {
       height = this.minHeight;
     }
-
     this.renderer.setStyle(this.el.nativeElement, 'height', `${height}px`);
+  }
+
+  ngOnDestroy(): void {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
   }
 }
